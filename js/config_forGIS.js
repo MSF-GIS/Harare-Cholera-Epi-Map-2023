7812d2a407448e74a9da6ade9614b395c5591fd1
lalapmufoyah <lalapmufoyah@gmail.com>
2023-05-29 21:12:55 +0200

    4th update

--- js/config_forGIS.js
+++ js/config_forGIS.js
@@ -21,7 +21,7 @@
 				// epiDataset - Related to the delimiter-separated values dataset to be joined to the geometry.
 				epiDataset : {
 					// source (STRING) - Link to the epi data, can be remote or local.
-					source : "data/harare_230523.csv",
+					source : "data/harare_230527.csv",
 					// delimiter (STRING) - Defines the delimiter used in the dataset.
 					delimiter : ",",
 	                // remote (BOOLEAN) -  To be set as 'true' for online use. Set as 'false' to avoid CORS issues on local use.
@@ -210,7 +210,7 @@
 			// Define layout components (all param : STRING) - Title, descriptions, etc...
 		    layout : {
 		        // appTitle - Set the title of the webmap.
-		        appTitle : "ZIMBABWE - Harare 2023 Cholera Outbreak (As of May 23, 2023)",
+		        appTitle : "ZIMBABWE - Harare 2023 Cholera Outbreak (As of May 27, 2023)",
 		        // analysisSectionTitle - Section title for analysis selector.
 		        analysisSectionTitle : "Map analysis",
 		        // analysisDescription - Description below analysis title (Use HTML tags for formatting).
